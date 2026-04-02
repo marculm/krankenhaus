42f051efaa2a0894947bdf67c0a4946d6716e5b5
marculm <marc.ulm24@gmail.com>
2026-04-02 16:09:32 +0200

    find_all + _count_all_rows

--- src/krankenhaus/repository/krankenhaus_repository.py
+++ src/krankenhaus/repository/krankenhaus_repository.py
@@ -3,10 +3,12 @@
 from typing import Final
 
 from loguru import logger
-from sqlalchemy import select
+from sqlalchemy import func, select
 from sqlalchemy.orm import Session, joinedload
 
 from krankenhaus.entity import Krankenhaus
+from krankenhaus.repository.pageable import Pageable
+from krankenhaus.repository.slice import Slice
 
 __all__ = ["KrankenhausRepository"]
 
@@ -37,3 +39,34 @@ class KrankenhausRepository:
         logger.debug("Krankenhaus: {}", krankenhaus)
 
         return krankenhaus
+
+    def find_all(self, pageable: Pageable, session: Session) -> Slice[Krankenhaus]:
+        """Alle Krankenhäuser mit Pagination suchen.
+
+        :param pageable: Pageable-Objekt mit Seitennummer und Anzahl pro Seite
+        :param session: SQLAlchemy Session
+        :return: Liste der gefundenen Krankenhäuser
+        """
+        statement: Final = (
+            (
+                select(Krankenhaus)
+                .options(joinedload(Krankenhaus.adresse))
+                .offset(pageable.number * pageable.size)
+                .limit(pageable.size)
+            )
+            if pageable.size != 0
+            else (select(Krankenhaus).options(joinedload(Krankenhaus.adresse)))
+        )
+
+        krankenhaeuser: Final = session.scalars(statement).all()
+        logger.debug("Krankenhäuser: {}", krankenhaeuser)
+        anzahl: Final = self._count_all_rows(session)
+        krankenhaus_slice: Final = Slice(
+            content=tuple(krankenhaeuser), total_elements=anzahl
+        )
+        return krankenhaus_slice
+
+    def _count_all_rows(self, session: Session) -> int:
+        statement: Final = select(func.count()).select_from(Krankenhaus)
+        count: Final = session.execute(statement).scalar()
+        return count if count is not None else 0
