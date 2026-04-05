2d131e5bf8f8f4c878dc95f68555b6064790e50d
Johannes <johannesgt44@gmail.com>
2026-04-05 19:08:50 +0200

    adresse model

---
+++ src/krankenhaus/router/adressse_model.py
@@ -0,0 +1,46 @@
+"""Pydantic-Model für die Adresse."""
+
+from typing import Annotated
+
+from pydantic import BaseModel, ConfigDict, StringConstraints
+
+from krankenhaus.entity import Adresse
+
+__all__ = ["AdresseModel"]
+
+
+class AdresseModel(BaseModel):
+    """Pydantic-Model für die Adresse."""
+
+    plz: Annotated[str, StringConstraints(pattern=r"^\d{5}$")]
+    """Postleitzahl"""
+    ort: Annotated[str, StringConstraints(max_length=64)]
+    """Ort"""
+    strasse: str
+    """Straße"""
+    hausnummer: str
+    """Hausnummer"""
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "plz": "99999",
+                "ort": "Testort",
+                "strasse": "Teststraße",
+                "hausnummer": "1a",
+            },
+        }
+    )
+
+    def to_adresse(self) -> Adresse:
+        """Konvertierung in ein Adresse-Objekt für SQLAlchemy.
+
+        :return: Adresse-Objekt für SQLAlchemy
+        :rtype: Adresse
+        """
+        adresse_dict = self.model_dump()
+        adresse_dict["id"] = None
+        adresse_dict["patient_id"] = None
+        adresse_dict["patient"] = None
+
+        return Adresse(**adresse_dict)
