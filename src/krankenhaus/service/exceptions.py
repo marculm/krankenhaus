fd58fc985b177a8e6455d6b88fdb8a65c08d9c91
marculm <marc.ulm24@gmail.com>
2026-04-05 18:22:30 +0200

    added NotFoundError

--- src/krankenhaus/service/exceptions.py
+++ src/krankenhaus/service/exceptions.py
@@ -1,5 +1,9 @@
 """Exceptions im Projekt Krankenhaus."""
 
+from collections.abc import Mapping
+
+__all__ = ["EmailExistsError"]
+
 
 class EmailExistsError(Exception):
     """Exception, falls die Emailadresse bereits existiert."""
@@ -11,3 +15,33 @@ class EmailExistsError(Exception):
         """
         super().__init__(f"Existierende Email: {email}")
         self.email = email
+
+
+class NotFoundError(Exception):
+    """Exception, falls kein Krankenhaus gefunden wurde."""
+
+    def __init__(
+        self,
+        krankenhaus_id: int | None = None,
+        suchparameter: Mapping[str, str] | None = None,
+    ) -> None:
+        """Initialisierung von NotFoundError mit ID und Suchparameter.
+
+        :param krankenhaus_id: Krankenhaus-ID, zu der nichts gefunden wurde
+        :param suchparameter: Suchparameter, zu denen nichts gefunden wurde
+        """
+        super().__init__("Not Found")
+        self.krankenhaus_id = krankenhaus_id
+        self.suchparameter = suchparameter
+
+
+class VersionOutdatedError(Exception):
+    """Exception, falls die Versionsnummer beim Aktualisieren veraltet ist."""
+
+    def __init__(self, version: int) -> None:
+        """Initialisierung von VersionOutdatedError mit veralteter Versionsnummer.
+
+        :param version: Veraltete Versionsnummer
+        """
+        super().__init__(f"Veraltete Version: {version}")
+        self.version = version
