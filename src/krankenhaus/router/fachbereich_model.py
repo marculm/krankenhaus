6ffbc75aa59614678fd9a7d4799d8b4532a6e139
marculm <marc.ulm24@gmail.com>
2026-04-09 09:50:00 +0200

    fix fachbereich_model

--- src/krankenhaus/router/fachbereich_model.py
+++ src/krankenhaus/router/fachbereich_model.py
@@ -11,9 +11,16 @@ class FachbereichModel(BaseModel):
     """Pydantic-Model für die Fachbereiche."""
 
     name: str
+    """Der Name des Fachbereichs."""
+
     beschreibung: str
+    """Die Beschreibung des Fachbereichs."""
+
     leitung: str
+    """Die Leitung des Fachbereichs."""
+
     anzahlaerzte: int
+    """Die Anzahl der Ärzte im Fachbereich."""
 
     model_config = ConfigDict(
         json_schema_extra={
@@ -27,10 +34,14 @@ class FachbereichModel(BaseModel):
     )
 
     def to_fachbereich(self) -> Fachbereich:
-        """Konvertiert das Pydantic-Model in ein Fachbereich-Entity-Objekt."""
+        """Konvertiert das Pydantic-Model in ein Fachbereich-Entity-Objekt.
+
+        :return: Fachbereich-Objekt für SQLAlchemy
+        :rtype: Fachbereich
+        """
         fachbereich_dict = self.model_dump()
-        fachbereich_dict["id"]
-        fachbereich_dict["krankenhausid"]
-        fachbereich_dict["krankenhaus"]
+        fachbereich_dict["id"] = None
+        fachbereich_dict["krankenhausid"] = None
+        fachbereich_dict["krankenhaus"] = None
 
         return Fachbereich(**fachbereich_dict)
