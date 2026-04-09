9314b07d6ad0c38aa59adf6bdbfc75a4c0908517
marculm <marc.ulm24@gmail.com>
2026-04-09 09:50:41 +0200

    add krankenhaus_update_model to_dict

--- src/krankenhaus/router/krankenhaus_update_model.py
+++ src/krankenhaus/router/krankenhaus_update_model.py
@@ -1,19 +1,47 @@
-"""Pydandic-Model zum Aktualisieren von Krankenhausdaten."""
-from pydantic import BaseModel
+"""Pydantic Models für das Updaten eines Krankenhaus."""
 
-__all__ = ["KrankenhausUpdateModel"]
+from typing import Any
+
+from pydantic import BaseModel, ConfigDict, EmailStr
 
 
 class KrankenhausUpdateModel(BaseModel):
-    """Pydantic Model zum Aktualisieren von Krankenhausdaten."""
+    """Pydantic Model für das Update eines Krankenhauses."""
 
     name: str
-    """Der zugehörige Name des Krankenhauses."""
+    """Der Name des Krankenhauses."""
 
     mitarbeiteranzahl: int
     """Die Anzahl der Mitarbeiter im Krankenhaus."""
 
     bettenanzahl: int
-
-    email: str
     """Die Anzahl der Betten im Krankenhaus."""
+
+    email: EmailStr
+    """Die einduetige E-Mail-Adresse des Krankenhauses."""
+
+    model_config = ConfigDict(
+        json_schema_extra={
+            "example": {
+                "nachname": "Test",
+                "mitarbeiteranzahl": 50,
+                "bettenanzahl": 100,
+                "email": "test@acme.com",
+            },
+        }
+    )
+
+    def to_dict(self) -> dict[str, Any]:
+        """Konvertiert das Pydantic Model in ein Dictionary.
+
+        :return: Dictionary mit den Attributen des Pydantic Models
+        :rtype: dict[str, Any]
+        """
+        krankenhaus_dict = self.model_dump()
+        krankenhaus_dict["id"] = None
+        krankenhaus_dict["adresse"] = None
+        krankenhaus_dict["fachbereich"] = []
+        krankenhaus_dict["erzeugt"] = None
+        krankenhaus_dict["aktualisiert"] = None
+
+        return krankenhaus_dict
