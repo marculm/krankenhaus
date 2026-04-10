700c1bf26c606d520d0c2dd70843c92d3a071159
marculm <marc.ulm24@gmail.com>
2026-04-10 18:56:57 +0200

    added create table adresse in create.sql

--- src/krankenhaus/config/resources/postgresql/create.sql
+++ src/krankenhaus/config/resources/postgresql/create.sql
@@ -1,6 +1,6 @@
 SET default_tablespace = krankenhausspace;
 
-CREATE TABLE IF NOT EXISTS krankenhaus(
+CREATE TABLE IF NOT EXISTS krankenhaus (
     id                  INTEGER GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY,
     version             INTEGER NOT NULL DEFAULT 0,
     name                TEXT NOT NULL,
@@ -14,4 +14,11 @@ CREATE TABLE IF NOT EXISTS krankenhaus(
 
 CREATE INDEX IF NOT EXISTS krankenhaus_name_idx ON krankenhaus(name);
 
-
+CREATE TABLE IF NOT EXISTS adresse (
+    id              INTEGER GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY,
+    strasse         TEXT NOT NULL,
+    hausnummer      TEXT NOT NULL,
+    plz             TEXT NOT NULL CHECK (plz ~ '\d{5}'),
+    ort             TEXT NOT NULL,
+    krankenhaus_id  INTEGER NOT NULL REFERENCES krankenhaus ON DELETE CASCADE
+)
