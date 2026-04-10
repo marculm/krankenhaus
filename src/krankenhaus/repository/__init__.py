c4cfd05d31cfbbed184f55e4d6cbe0a6e944b8e8
marculm <marc.ulm24@gmail.com>
2026-04-10 18:33:22 +0200

    added session_factory in src/krankenhaus/repository

--- src/krankenhaus/repository/__init__.py
+++ src/krankenhaus/repository/__init__.py
@@ -2,6 +2,14 @@
 
 from .krankenhaus_repository import KrankenhausRepository
 from .pageable import MAX_PAGE_SIZE, Pageable
+from .session_factory import Session, engine
 from .slice import Slice
 
-__all__: list[str] = ["MAX_PAGE_SIZE", "KrankenhausRepository", "Pageable", "Slice"]
+__all__: list[str] = [
+    "MAX_PAGE_SIZE",
+    "KrankenhausRepository",
+    "Pageable",
+    "Session",
+    "Slice",
+    "engine"
+]
