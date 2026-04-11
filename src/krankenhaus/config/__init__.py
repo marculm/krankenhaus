48e4e3d7d074c24f3f312654cb711779e22a7995
marculm <marc.ulm24@gmail.com>
2026-04-11 18:46:58 +0200

    fix init.py in config

--- src/krankenhaus/config/__init__.py
+++ src/krankenhaus/config/__init__.py
@@ -1,16 +1,15 @@
 """Modul zur Konfiguration."""
 
-#from krankenhaus.config.db import ( #noqa
-#    db_connect_args,
-#    db_dialect,
-#    db_log_statements,
-#    db_url,
-#    db_url_admin,
-#) # noqa
-#from krankenhaus.config.graphql import graphql_ide # noqa
-
+from krankenhaus.config.db import (
+    db_connect_args,
+    db_dialect,
+    db_log_statements,
+    db_url,
+    db_url_admin,
+)
 from krankenhaus.config.dev_modus import dev_db_populate, dev_keycloak_populate
 from krankenhaus.config.excel import excel_enabled
+from krankenhaus.config.graphql import graphql_ide
 from krankenhaus.config.keycloak import (
     csv_config,
     keycloak_admin_config,
@@ -29,15 +28,15 @@ from krankenhaus.config.tls import tls_certfile, tls_keyfile
 __all__ = [
     "config_logger",
     "csv_config",
-#    "db_connect_args",
-#    "db_dialect",
-#    "db_log_statements",
-#    "db_url",
-#    "db_url_admin",
+    "db_connect_args",
+    "db_dialect",
+    "db_log_statements",
+    "db_url",
+    "db_url_admin",
     "dev_db_populate",
     "dev_keycloak_populate",
     "excel_enabled",
-#    "graphql_ide",
+    "graphql_ide",
     "host_binding",
     "keycloak_admin_config",
     "keycloak_config",
