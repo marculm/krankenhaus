2ccac9d28f7903ee47d80ba55c3c18f7650180ba
marculm <marc.ulm24@gmail.com>
2026-04-11 18:47:23 +0200

    fix dependencies

--- src/krankenhaus/router/dependencies.py
+++ src/krankenhaus/router/dependencies.py
@@ -5,9 +5,7 @@ from typing import Annotated
 from fastapi import Depends
 
 from krankenhaus.repository.krankenhaus_repository import KrankenhausRepository
-from krankenhaus.service.krankenhaus_write_service import KrankenhausWriteService
-
-# from krankenhaus.service.krankenhaus_service import KrankenhausService
+from krankenhaus.service import KrankenhausService, KrankenhausWriteService
 
 
 def get_repository() -> KrankenhausRepository:
@@ -19,11 +17,11 @@ def get_repository() -> KrankenhausRepository:
     return KrankenhausRepository()
 
 
-# def get_service(
-#     repo: Annotated[KrankenhausRepository, Depends(get_repository)],
-# ) -> KrankenhausService:
-#     """Factory-Funktion für KrankenhausService."""
-#     return KrankenhausService(repo=repo)
+def get_service(
+    repo: Annotated[KrankenhausRepository, Depends(get_repository)],
+) -> KrankenhausService:
+    """Factory-Funktion für KrankenhausService."""
+    return KrankenhausService(repo=repo)
 
 
 def get_write_service(
