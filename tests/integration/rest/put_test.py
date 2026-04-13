ac8283a61beb9f617b6986120a7feab95a17b416
marculm <marc.ulm24@gmail.com>
2026-04-13 22:26:58 +0200

    added test_put

---
+++ tests/integration/rest/put_test.py
@@ -0,0 +1,43 @@
+# ruff: noqa: D103, S101
+"""Tests für PUT-Requests."""
+
+from http import HTTPStatus
+from typing import Final
+
+from common_test import ctx, login, rest_url
+from httpx import put
+from pytest import mark
+
+EMAIL_UPDATE: Final = "put@test.de"
+
+
+@mark.rest
+@mark.put_request
+def test_put() -> None:
+    # arrange
+    krankenhaus_id: Final = 10
+    if_match: Final = '"0"'
+    geaendertes_krankenhaus: Final = {
+        "name": "Testkrankenhaus",
+        "mitarbeiteranzahl": 100,
+        "bettenanzahl": 200,
+        "email": EMAIL_UPDATE
+    }
+    token: Final = login()
+    assert token is not None
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "If-Match": if_match
+    }
+
+    # act
+    response: Final = put(
+        f"{rest_url}/{krankenhaus_id}",
+        json=geaendertes_krankenhaus,
+        headers=headers,
+        verify=ctx
+    )
+
+    # assert
+    assert response.status_code == HTTPStatus.NO_CONTENT
+    assert not response.text
