7892f7a4cc609de4cdd78f8ceab0b6a3cd533c17
marculm <marc.ulm24@gmail.com>
2026-04-13 22:43:54 +0200

    added test_put_nicht_vorhanden

--- tests/integration/rest/put_test.py
+++ tests/integration/rest/put_test.py
@@ -41,3 +41,34 @@ def test_put() -> None:
     # assert
     assert response.status_code == HTTPStatus.NO_CONTENT
     assert not response.text
+
+
+@mark.rest
+@mark.put_request
+def test_put_nicht_vorhanden() -> None:
+    # arrange
+    krankenhaus_id: Final = 00
+    if_match: Final = '"0"'
+    geaendertes_krankenhaus: Final = {
+        "name": "Testkrankenhaus",
+        "mitarbeiteranzahl": 100,
+        "bettenanzahl": 200,
+        "email": EMAIL_UPDATE
+    }
+    token: Final = login()
+    assert token is not None
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "If-Match": if_match
+    }
+
+    # act
+    response: Final = put(
+        f"{rest_url}/{krankenhaus_id}",
+        json=geaendertes_krankenhaus,
+        headers=headers,
+        verify=ctx
+    )
+
+    # assert
+    assert response.status_code == HTTPStatus.NOT_FOUND
