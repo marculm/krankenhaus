4b84e2b4d73844b115c9e6fb96e0f1f8cbc54548
Johannes <johannesgt44@gmail.com>
2026-04-14 14:22:12 +0200

    added rest get_by_id test

---
+++ tests/integration/rest/get_by_id_test.py
@@ -0,0 +1,36 @@
+# ruff: noqa: S101
+"""Tests für GET mit Query-Parameter."""
+
+from http import HTTPStatus
+from typing import Final
+
+from common_test import ctx, login, rest_url
+from httpx import get
+from pytest import mark
+
+
+# in pyproject.toml bei der Table [tool.pytest.ini_options] gibt es das Array "markers"
+@mark.rest
+@mark.get_request
+@mark.parametrize("krankenhaus_id", [10, 20, 30])
+def test_get_by_id(krankenhaus_id: int) -> None:
+    """Test für GET mit Query-Parameter, wenn der Nutzer ein Admin ist."""
+    # arrange
+    token: Final = login()
+    assert token is not None
+    headers: Final = {"Authorization": f"Bearer {token}"}
+
+    # act
+    response: Final = get(
+        f"{rest_url}/{krankenhaus_id}",
+        headers=headers,
+        verify=ctx,
+    )
+
+    # assert
+    assert response.status_code == HTTPStatus.OK
+    response_body: Final = response.json()
+    assert isinstance(response_body, dict)
+    id_actual: Final = response_body.get("id")
+    assert id_actual is not None
+    assert id_actual == krankenhaus_id
